92e1d6fbad660dca902398afeec042dd44c4a7e7
brsoff <brsoff@gmail.com>
2014-02-25 17:58:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WdiProject3::Application.config.secret_key_base = '02c01f9188d16b0927ad23d32c60b15f54ec764906bffcc9f9205cb90a41108d05c25c029678a3966f936353c1db79e4631c1974ba56e8761d7cca1d3e0356d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WdiProject3::Application.config.session_store :cookie_store, key: '_wdi_project_3_session'

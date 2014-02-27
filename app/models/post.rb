52c135bcaf461a6ce3eb1cced98057a1e46ef462
Matthew Plummer <plummat@gmail.com>
2014-02-27 15:26:21 -0500

    merge stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,32 @@ class Post < ActiveRecord::Base
 
   def self.get_posts(current_user)
     user = User.find(current_user.id)
-    followed_users = user.followed_users
+    array = user.followed_users
+    binding.pry
+
+    array << user
     @posts = Array.new
-    followed_users.each do |user|
-      user.posts.each do |post|
+    array.each do |followed_user|
+      followed_user.posts.each do |post|
         @posts << post
       end
     end
     return @posts
   end
+
+  def self.add_post(params, user)
+    @post = Post.create(message: params[:message], user_id: user.id)
+    @item = Item.create(name: params[:item_name], post_id: @post.id, url: params[:item_url], itemtype_id: params[:item_type])
+    return @post
+  end
+
+
+  #  {"message"=>"omg does this work",
+  # "item_name"=>"community",
+  # "item_url"=>"nbc.com",
+  # "item_type"=>"3",
+  # "action"=>"create",
+  # "controller"=>"posts",
+  # "post"=>{"message"=>"omg does this work"}}
 
 end

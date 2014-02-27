c98326f99449f2af67bcaddfbff9bf0ce14c8905
Eric Streske <estreske@gmail.com>
2014-02-26 19:52:36 -0500

    added get_posts method to user class and set up home controller to render json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,15 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :except => [:index]
 
   def index
-    user = User.find(current_user.id)
-    render json: Post.all
+    if current_user && current_user.followed_users.length > 0
+      @posts = User.get_posts(current_user)
+    else
+      @posts = ["You are not following anybody."]
+    end
+    respond_to do |format|
+      format.json {render json: @posts}
+      format.html
+    end
   end
-
+  
 end

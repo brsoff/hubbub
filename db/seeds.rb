261e20a9a23477407f9e21069827dd8ad1786d9f
brsoff <brsoff@gmail.com>
2014-03-01 11:24:44 -0500

    clean up seed file, add avatar url in seed file, changed buttons on posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,9 @@
-
-
-# Itemtype.delete_all
 Post.delete_all
-# Item.delete_all
 User.delete_all
 Follow.delete_all
 
-# Itemtype.create(name: "Book")
-# Itemtype.create(name: "Movie")
-# Itemtype.create(name: "TV Show")
-# Itemtype.create(name: "Product")
-# restaurant = Itemtype.create(name: "Restaurant")
-# Itemtype.create(name: "Link")
-# Itemtype.create(name: "Article")
-
-bob = User.create(name: "Bob Bobertson", username: "bbob", email: "bobertson87@gmail.com", password: "password123")
-kevin = User.create(name: "Kevin Smith", username: "ksmith", email: "ksmith@gmail.com", password: "password123")
-
-# portillos = Item.create(itemtype_id: restaurant.id, url: "http://www.portillos.com/", name: "Portillos")
-
+bob = User.create(name: "Bob Bobertson", username: "bbob", email: "bobertson87@gmail.com", password: "password123", avatar_url: "http://placebear.com/200/200")
+kevin = User.create(name: "Kevin Smith", username: "ksmith", email: "ksmith@gmail.com", password: "password123", avatar_url: "http://placedog.com/200/200")
 portillos_post = Post.create(message: "Great sandwiches!", item_category: "Restaurant", item_url: "http://www.portillos.com", item_name: "Portillos" )
 portillos_post.user_id = bob.id
 portillos_post.save!

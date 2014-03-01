eeb37bba73ca0b61ee0920ea3a9b77086db7a33a
brsoff <brsoff@gmail.com>
2014-03-01 13:34:20 -0500

    added edge case so user cant unfollow themselves

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,14 +1,24 @@
 class FollowsController < ApplicationController
 
   def follow
-   @follow = Follow.create(follower_id: params[:current_user_id], :followed_id: params[:follow_id])
+    current_user = params[:current_user_id]
+    followed_user = params[:unfollow_id]
+
+   @follow = Follow.create(follower_id: current_user, :followed_id: followed_user)
     render json: @follow
   end
 
   def unfollow
-    @unfollow = Follow.where(follower_id: params[:current_user_id], :followed_id: params[:unfollow_id])
-    @unfollow.destroy
-    render json: @unfollow
+    current_user = params[:current_user_id]
+    followed_user = params[:unfollow_id]
+
+    if current_user != followed_user
+      @unfollow = Follow.where(follower_id: current_user, :followed_id: followed_user)
+      @unfollow.destroy
+      render json: @unfollow
+    else
+      puts "uhhh"
+    end
   end
 
 end

21756b00db61b6e7c61c4637b6f634bcb5901bf1
Eric Streske <estreske@gmail.com>
2014-03-01 13:35:21 -0500

    added follow and unfollow methods to searchuserview and edited template for search results

--- app/assets/javascripts/views/searchuser.js
+++ app/assets/javascripts/views/searchuser.js
@@ -9,7 +9,54 @@ SearchUserView = Backbone.View.extend({
   },
 
   events: {
-  
+    "click .unfollow": "unfollow",
+    "click .follow": "follow"
+  },
+
+  follow: function(){
+    console.log("clicked follow")
+    var current_user_id = Hubub.currentuser.attributes.user_id
+    console.log(current_user_id)
+    var follow_id = $('.user_search_data').attr("data-id");
+    console.log(follow_id)
+
+    var params = {
+      current_user_id: current_user_id,
+      follow_id : follow_id
+    }
+
+    $.ajax({
+      url: "/follow",
+      method: "post",
+      data: params,
+      dataType: "json",
+      success: function(data){
+        console.log(data)
+      }
+    })
+  },
+
+  unfollow: function() {
+    console.log("clicked unfollow")
+    var current_user_id = Hubub.currentuser.attributes.user_id
+    console.log(current_user_id)
+    var unfollow_id = $('.user_search_data').attr("data-id");
+    console.log(unfollow_id)
+
+    var params = {
+      current_user_id: current_user_id,
+      unfollow_id : unfollow_id
+    }
+
+    $.ajax({
+      url: "/unfollow",
+      method: "post",
+      data: params,
+      dataType: "json",
+      success: function(data){
+        console.log(data)
+      }
+    })
   },
 
   render: function (){

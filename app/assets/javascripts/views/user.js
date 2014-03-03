c2b2016c4b56a0c7ea54ead9c3a8c198124d7338
brsoff <brsoff@gmail.com>
2014-03-03 16:47:58 -0500

    added working follow/unfollow buttons and functionality on noncurrent user pages

--- app/assets/javascripts/views/user.js
+++ app/assets/javascripts/views/user.js
@@ -11,6 +11,92 @@ UserView = Backbone.View.extend({
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+
+  events: {
+    "click .unfollow": "unfollow",
+    "click .follow": "follow"
+  },
+
+  follow: function(){
+    var current_user_id = Hubbub.currentuser.attributes.user_id
+    var follow_id = $('.user-info').attr("data-id");
+    var params = {
+      current_user_id: current_user_id,
+      follow_id : follow_id
+    }
+
+    $.ajax({
+      url: "/follow",
+      method: "post",
+      data: params,
+      dataType: "json",
+      success: function(data){
+        // Hubbub.show(Hubbub.user.attributes.username)
+        $.ajax({
+          url: "/userdata",
+          data: {username: Hubbub.user.attributes.username},
+          type: "get",
+          dataType: "json",
+          success: function (data) {
+            Hubbub.currentuser.fetch({
+              success: function () {
+                Hubbub.currentuserposts.fetch({
+                  success: function () {
+                  Hubbub.user = new User (data)
+                  Hubbub.userview = new UserView({model: Hubbub.user});
+                  $("#current_user_container").empty();
+                  $("#current_user_container").html(Hubbub.userview.render().el)
+                  Hubbub.userview.delegateEvents();
+                  }
+                })
+              }
+            });
+          } 
+        })
+      }
+    })
+  },
+
+  unfollow: function() {
+    var current_user_id = Hubbub.currentuser.attributes.user_id;
+    var unfollow_id = $('.user-info').attr("data-id");
+
+    var params = {
+      current_user_id: current_user_id,
+      unfollow_id : unfollow_id
+    }
+
+    $.ajax({
+      url: "/unfollow",
+      method: "post",
+      data: params,
+      dataType: "json",
+      success: function(data){
+       $.ajax({
+          url: "/userdata",
+          data: {username: Hubbub.user.attributes.username},
+          type: "get",
+          dataType: "json",
+          success: function (data) {
+            Hubbub.currentuser.fetch({
+              success: function () {
+                Hubbub.currentuserposts.fetch({
+                  success: function () {
+                  Hubbub.user = new User (data)
+                  Hubbub.userview = new UserView({model: Hubbub.user});
+                  $("#current_user_container").empty();
+                  $("#current_user_container").html(Hubbub.userview.render().el)
+                  Hubbub.userview.delegateEvents();
+                  }
+                })
+              }
+            })
+          } 
+        })       
+      }
+    })
+    
   }
 
 

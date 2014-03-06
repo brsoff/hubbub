7fc83a01b0d9a4e2e2cb3ca8a37bc560758dddaf
brsoff <brsoff@gmail.com>
2014-03-06 17:44:42 -0500

    refactoring post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,111 +1,82 @@
 require 'spec_helper'
 
-describe "#self.format_url" do 
+describe "Post model" do
+
+  describe "#self.format_url" do
+    before do
+      @url1 = "http://www.portillos.com"
+      @url2 = "portillos.com"
+      @url3 = ""
+      @url4 = "www.portillos.com"
+      @url5 = nil
+    end
+
+    it "should format urls properly" do
+      Post.format_url(@url1).should == "http://www.portillos.com"
+      Post.format_url(@url2).should == "http://portillos.com"
+      Post.format_url(@url3).should == ""
+      Post.format_url(@url4).should == "http://www.portillos.com"
+      Post.format_url(@url5).should == nil
+    end
+  end
+
+  describe "#self.add_posts" do
+    before do
+      @current_user = FactoryGirl.create(:user)
+      new_post = FactoryGirl.create(:post)
+      @post = Post.add_post(new_post, @current_user)
+    end
+
+    it "should add post to the database" do
+      Post.last.should == @post
+    end
+
+    it "should belong to the user" do
+      Post.last.user_id.should == @current_user.id
+    end
+  end
+
+
+  describe "#self.get_posts" do
+    before do
+      ### create some fake users with fake posts and have the current user follow them
+      current_user = FactoryGirl.create(:user)
+      new_post = FactoryGirl.create(:post)
+      fake_user = FactoryGirl.create(:user, email: "example@example.com")
+      fake_user2 = FactoryGirl.create(:user, email: "example2@example2.com")
+      follow1 = FactoryGirl.create(:follow, followed_id: fake_user.id, follower_id: current_user.id)
+      follow2 = FactoryGirl.create(:follow, followed_id: fake_user2.id, follower_id:current_user.id)
+      @post_1 = Post.add_post(new_post, fake_user)
+      sleep 1
+      new_post_2 = FactoryGirl.create(:post)
+      @post_2 = Post.add_post(new_post_2, fake_user2)
+      @posts = Post.get_posts(current_user)
+
+    end
+
+    it "should return all user's posts and followed user's posts" do
+      @posts.should == [@post_1, @post_2]
+    end
+
+    xit "should order posts by created_at time in reverse" do
+      Post.order_posts(@posts).should == [@post_2, @post_1]
+    end
+  end
+
+end
+
+
+describe "#self.get_watched" do
   before do
-    @url1 = "http://www.portillos.com"
-    @url2 = "portillos.com"
-    @url3 = ""
-    @url4 = "www.portillos.com"
-    @url5 = nil
-  end
-
-  it "should format urls properly" do 
-    Post.format_url(@url1).should == "http://www.portillos.com"
-    Post.format_url(@url2).should == "http://portillos.com"
-    Post.format_url(@url3).should == ""
-    Post.format_url(@url4).should == "http://www.portillos.com"
-    Post.format_url(@url5).should == nil
-  end
-end
-
-describe "#self.add_posts" do 
-  before do 
     @current_user = FactoryGirl.create(:user)
-    @params = { 
-      id: 1,
-      message: "Great sandwiches!",
-      user_id: @current_user.id,
-      item_url: "http://www.portillos.com",
-      item_category: "Restaurant",
-      item_name: "Portillos",
-      item_image_url: nil,
-      user_name: "Bob Bobertson", 
-      username: "bbob"
-    }
-    @post = Post.add_post(@params, @current_user)
-  end 
+    fake_user = FactoryGirl.create(:user, email: "example@example.com")
+    new_post = FactoryGirl.create(:post, user_id: fake_user.id)
+    post = Post.add_post(new_post, fake_user)
 
-  it "should add post to the database" do
-    Post.last.should == @post
-  end
-end
-
-describe "#self.get_posts" do 
-  before do 
-    Post.delete_all
-    @current_user = FactoryGirl.create(:user)
-    @fake_user = User.create(id: 2, name: "Brendan", username: "brsoff", email: "bsoff@gmail.com", password: "password123", password_confirmation: "password123")
-    @fake_user2 = User.create(id: 3, name: "Eric", username: "estreske", email: "es@gmail.com", password: "password123", password_confirmation: "password123")
-    @follow1 = Follow.create(followed_id: @fake_user.id, follower_id: @current_user.id)
-    @follow2 = Follow.create(id: 2, followed_id: @fake_user2.id, follower_id: @current_user.id)
-    @params = {
-      message: "Great sandwiches!",
-      user_id: @fake_user.id,
-      item_url: "http://www.portillos.com",
-      item_category: "Restaurant",
-      item_name: "Portillos",
-      item_image_url: nil,
-      user_name: "Bob Bobertson", 
-      username: "bbob"
-    }
-    @post = Post.add_post(@params, @fake_user)
-
-    sleep 2
-
-    @params2 = {
-      message: "Delicious!",
-      user_id: @fake_user2.id,
-      item_url: "http://steak.com",
-      item_category: "Food",
-      item_name: "Steak",
-      item_image_url: "",
-      user_name: "Eric",
-      username: "estreske"
-    }
-    @post2 = Post.add_post(@params2, @fake_user2)
-    @posts = Post.get_posts(@current_user) 
+    @watchlist = Watchlist.create(post_id: post.id, user_id: @current_user.id)
   end
 
-  it "should return all user's posts and followed user's posts" do 
-    @posts.should == [@post, @post2]
-  end
-
-  it "should order posts by created_at time" do 
-    Post.order_posts(@posts).should == [@post2, @post]
-  end
-end
-
-
-describe "#self.get_watched" do 
-  before do 
-    @current_user = FactoryGirl.create(:user)
-    @fake_user = User.create(id: 2, name: "Brendan", username: "brsoff", email: "bsoff@gmail.com", password: "password123", password_confirmation: "password123")
-    @params = {
-      message: "Great sandwiches!",
-      user_id: @fake_user.id,
-      item_url: "http://www.portillos.com",
-      item_category: "Restaurant",
-      item_name: "Portillos",
-      item_image_url: nil,
-      user_name: "Bob Bobertson", 
-      username: "bbob"
-    }
-    @post = Post.add_post(@params, @fake_user)
-
-    @watchlist = Watchlist.create(post_id: @post.id, user_id: @current_user.id)
-  end
-
-  it "should return a list of watchlisted items" do 
+  it "should return a list of watchlisted items" do
     Post.get_watched(@current_user).should == [@watchlist]
   end
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,10 +1,25 @@
-FactoryGirl.define do 
-  factory :user do 
-    id 1
+FactoryGirl.define do
+  factory :user do
     username "test_person"
     name "Bob Bobertson"
     email "bobertson87@gmail.com"
     password "password123"
     password_confirmation "password123"
   end
+
+  factory :post do
+    message "Great sandwiches!"
+    item_url "http://www.portillos.com"
+    item_category "Restaurant"
+    item_name "Portillos"
+    item_image_url nil
+    user_name "Bob Bobertson"
+    username "bbob"
+  end
+
+  factory :follow do
+    followed_id 1
+    follower_id 2
+  end
+
 end

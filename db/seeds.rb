a67e6123a1a79a387cb122557137f63577fb4270
Matthew Plummer <plummat@gmail.com>
2014-03-16 15:17:58 -0400

    fixed unfollow bug for now... something with fetching posts in the unfollow chainr eturns an error, probably something with how we coded the render function in userposts. I think just incrementing and changing button class is preferable to doing all those fetches to the server, but it works!  also removed exception handler because we have users autofollow themselved on the registrations controller now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@ Post.delete_all
 User.delete_all
 Follow.delete_all
 
+eric = User.create(name: "Eric", username: "eric", email: "eric87@gmail.com", password: "password123", avatar_url: "http://www.godairyfree.org/wp-content/uploads/2007/06/sodeliciousminis2.jpg")
+seed_post = Post.create(message: "Great SUSGHJDS!", item_category: "Restaurant", item_url: "http://www.portillos.com", item_name: "Mexican" )
+seed_post.user_id = eric.id
+seed_post.user_name = eric.name
+seed_post.username = eric.username
+seed_post.save!
+
+
 bob = User.create(name: "Bob Bobertson", username: "bbob", email: "bobertson87@gmail.com", password: "password123", avatar_url: "http://www.godairyfree.org/wp-content/uploads/2007/06/sodeliciousminis2.jpg")
 kevin = User.create(name: "Kevin Smith", username: "ksmith", email: "ksmith@gmail.com", password: "password123", avatar_url: "http://www.stepbystep.com/wp-content/uploads/2013/04/Top-10-Most-Delicious-Sandwiches-in-the-World-6.jpg")
 portillos_post = Post.create(message: "Great sandwiches!", item_category: "Restaurant", item_url: "http://www.portillos.com", item_name: "Portillos" )
